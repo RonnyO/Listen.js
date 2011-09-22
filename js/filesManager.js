1109448a8c76e03e4a23b044fd0dbaf4fc15f6c1
Nir Geier <nirgeier@gmail.com>
2011-09-22 10:47:11 +0300

    Tree representation. MP3 tag V1

--- js/filesManager.js
+++ js/filesManager.js
@@ -1,51 +1,69 @@
 /**
- * This file will contain the FileSystem demo API + Logic
- * Author: Nir Geier
- *
- * The code is under no license.
- * You are free to do whatever you want with it.
+ * @class This file will contain the FileSystem demo API + Logic<br/>
+ * @author Nir Geier<br/>
+ * <br/>
+ * The code is under no license.<br/>
+ * You are free to do whatever you want with it.<br/>
  */
 var FilesManager = function() {
 
     // private section
 
-    // The files that we are processing
+    /**
+     * The array fo files that were found in the given directory
+     *
+     * @type {Array} type
+     * @field The field
+     * @private private
+     */
     var files;
 
-    // The tree representation
+    /**
+     * The jQuery tree plugin to display the file as tree
+     *
+     * @type {jQuery.tree}
+     * @field The field
+     * @private private
+     */
     var tree;
 
-    // The code settings
+    /**
+     * Object with the different settings/defaults that the user can change
+     *
+     * @type {Object}
+     * @field The field
+     * @private private
+     */
     var settings = {
 
-        // The file input field id
+        /** The file input field id */
         ID_FILE_INPUT : 'folderInput'
 
     };
 
     /**
-     * This method will set up the events, listeners and any other bindings that we might need
-     * later on in our code
+     * This method will set up the events, listeners and any other bindings that we might need later on in our code
+     *
+     * @private private
      */
     function setBindings() {
 
         // Add the event listener to the folder selector.
         // Once we select the folder we want to load adn store the files in it,
-        document.querySelector('#' + settings.ID_FILE_INPUT).addEventListener("change",
-            function (e) {
-                console.log('(#' + settings.ID_FILE_INPUT + ').addEventListener');
-                console.log(tree);
+        document.querySelector('#' + settings.ID_FILE_INPUT).addEventListener("change", function(e) {
+            console.log('(#' + settings.ID_FILE_INPUT + ').addEventListener');
+            console.log(tree);
 
-                // Read the files and display them as tree
-                tree.init(e);
+            // Read the files and display them as tree
+            tree.init(e);
 
-                // Process the files in the directory
-                processDirectory();
-            }, false);
+            // Process the files in the directory
+            processDirectory();
+        }, false);
     }
 
     /**
-     ** Read the metadata of the files in the given directory
+     * Read the files in the choosen directory
      */
     function processDirectory() {
         console.log('processDirectory');
@@ -62,27 +80,30 @@ var FilesManager = function() {
     return {
 
         /**
-         * Ant init/setup logic we will place in here
+         * Init the FilesManager. <br/>
+         * Here we will call the binding method and will init our tree
+         *
+         * @public
+         * @param {String [optional]} [selector]  The selector of the dom element to display the results inside
          */
-        init: function() {
+        init : function() {
             // Set the needed events/listeners/binding
             setBindings();
 
+            // init the tree
             tree = new FilesManager.Tree('#dir-tree');
 
         },
 
-        /**
-         * "API" calls
-         */
+        /** "API" calls */
 
         /**
          * Get the list of files in the directory
          *
-         * @return {Array}  files   List of files
+         * @return {Array}  files   the list of the current processed files in the chosen directory
          */
-        getFiles:function() {
-
+        getFiles : function() {
+            return files;
         }
     }; //return;
 }();
--- js/filesManagerTree.js
+++ js/filesManagerTree.js
@@ -1,5 +1,7 @@
 /**
- ** This file will handle the tree representation of the files dir
+ * @class This class is responsible for displaying the files as tree.<br/>
+ * <br/>
+ * @author Nir Geier<br/>
  **/
 
 /**
@@ -8,62 +10,141 @@
  */
 FilesManager.Tree = function(selector) {
 
-    // The tree dom node
+    /**
+     * The output dom selector for printing out the files as tree
+     *
+     * @type {Array} type
+     * @field The field
+     * @private
+     */
     var domObject = $(selector);
+
+    /**
+     * The files that this tree is displaying
+     *
+     * @type {Array}
+     * @field
+     * @private
+     */
     var files = [];
-    var listHtml = [];
-    var tree_ = {};
-    var pathList_ = [];
+
+    /**
+     * The html that contains the details fo all the files. The html is Array of dom elements
+     *
+     * @type {Array}
+     * @field
+     * @private
+     */
+    var outputHtml = [];
+
+
+    /**
+     * The tree structure of teh files.<br/>
+     * The tree is a (JSON) object that store the files and folder in levels as in real tree,
+     *
+     * @type {Object}
+     * @field
+     * @private
+     */
+    var tree = {};
+
+    /**
+     * Generate the tree html representation of the selected path. <br/>
+     *
+     * @param {Array}  pathList    Array of path(s) of all files in the selected directory
+     */
+    function pathToHtml(pathList) {
+        var folder;
+
+        // Verify that we have any data to process
+        if (pathList) {
+
+            // Loop over every path entry and process it.
+            for (folder in  pathList) {
+
+                // safety
+                if (pathList.hasOwnProperty(folder)) {
+
+                    // Check if teh entry if folder or file
+                    // file's will have a null value
+                    if (pathList[folder]) {
+                        // Add a new folder entry
+                        outputHtml.push('<li><a href="#">', folder, '</a>');
+
+                        // Sub list for the new folder
+                        outputHtml.push('<ul>');
+
+                        // recursive. Generate html for the current folder
+                        pathToHtml(pathList [folder]);
+
+                        // Folder is processed. close the sub list
+                        outputHtml.push('</ul>');
+                    } else {
+                        // file, set the html5 "custom data attributes" to file
+                        outputHtml.push('<li><a href="#" data-type="file">', folder, '</a></li>');
+                    }  // else
+                } // hasOwnProperty
+            } // for loop
+        }  //  if (pathList)
+    }
+
+    /**
+     * Build the tree from the given path(s)
+     *
+     * @param paths
+     */
+    function buildTree(paths) {
+
+        var i,path, subTree;
+
+        subTree = tree;
+        // JS5 forEach loop
+        paths.forEach(function(path /*, index, originalArray */) {
+
+            // Loop over each directory in the given path
+            path.split('/').forEach(function(folder /*, index, originalArray */) {
+                // Check to see if this folder was initialized already or not
+                if (!subTree[folder] && folder !== ".") {
+
+                }
+            });
+
+        });
+        for (i = 0; path = paths[i]; ++i) {
+            var pathParts = path.split('/');
+            var subObj = tree;
+            for (var j = 0, folderName; folderName = pathParts[j]; ++j) {
+                if (!subObj[folderName] && folderName != '.') {
+                    subObj[folderName] = j < pathParts.length - 1 ? {} : null;
+                }
+                subObj = subObj[folderName];
+            }
+        }
+        return tree;
+    }
 
     return{
 
-        render : function(object) {
-            if (object) {
-                for (var folder in object) {
-                    if (!object[folder]) { // file's will have a null value
-                        listHtml.push('<li><a href="#" data-type="file">', folder, '</a></li>');
-                    } else {
-                        listHtml.push('<li><a href="#">', folder, '</a>');
-                        listHtml.push('<ul>');
-                        this.render(object[folder]);
-                        listHtml.push('</ul>');
-                    }
-                }
-            }
-        },
-
-        buildFromPathList : function(paths) {
-            for (var i = 0, path; path = paths[i]; ++i) {
-                var pathParts = path.split('/');
-                var subObj = tree_;
-                for (var j = 0, folderName; folderName = pathParts[j]; ++j) {
-                    if (!subObj[folderName] && folderName != '.') {
-                        subObj[folderName] = j < pathParts.length - 1 ? {} : null;
-                    }
-                    subObj = subObj[folderName];
-                }
-            }
-            return tree_;
-        },
-
         init : function(e) {
+
+            var pathList = [];
+
             // Reset
-            listHtml = [];
-            tree_ = {};
-            pathList_ = [];
+            outputHtml = [];
+            tree = {};
             files = e.target.files;
 
             // TODO: optimize this so we're not going through the file list twice
-            // (here and in buildFromPathList).
+            // (here and in buildTree).
             var output = [];
             for (var i = 0, file; file = files[i]; ++i) {
-                pathList_.push(file.webkitRelativePath);
+                pathList.push(file.webkitRelativePath);
                 output.push(file.webkitRelativePath);
             }
 
-            this.render(this.buildFromPathList(pathList_));
+            pathToHtml(buildTree(pathList));
 
-            domObject.html(listHtml.join('')).tree({
+            domObject.html(outputHtml.join('')).tree({
                 expanded: 'li:first'
             });
 
@@ -72,6 +153,30 @@ FilesManager.Tree = function(selector) {
             for (var i = 0, fileNode; fileNode = fileNodes[i]; ++i) {
                 fileNode.dataset['index'] = i;
             }
+        },
+
+        readFile:function() {
+            document.querySelector('input[type="file"]').onchange = function(e) {
+                var reader = new FileReader();
+
+                reader.onload = function(e) {
+                    var dv = new DataView(this.result);
+
+                    // "TAG" starts at byte -128 from EOF.
+                    // See http://en.wikipedia.org/wiki/ID3
+                    if (dv.getString(3, dv.length - 128) == 'TAG') {
+                        var title = dv.getString(30, dv.tell());
+                        var artist = dv.getString(30, dv.tell());
+                        var album = dv.getString(30, dv.tell());
+                        var year = dv.getString(4, dv.tell());
+                    } else {
+                        // no ID3v1 data found.
+                    }
+                };
+
+                reader.readAsArrayBuffer(this.files[0]);
+            };
         }
+
     };
 };
